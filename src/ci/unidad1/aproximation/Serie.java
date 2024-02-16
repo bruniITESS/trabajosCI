e9a14372acc3d67c865ef42e4282302e1a4b76c4
Bruno <brunitosi123@gmail.com>
2024-02-16 13:38:09 -0600

    Clase serie1

--- src/ci/unidad1/aproximation/Serie.java
+++ src/ci/unidad1/aproximation/Serie.java
@@ -32,12 +32,15 @@ public class Serie {
         for (int i = iIni; i <= iEnd; i++){
             this.i[index] = i;
             f[index] = 2*i*i - 2*i;
+            suma += f[index];
             index++;
         }
     }
     public void print(){
         System.out.println("index  |  i  |   f");
-        for (int index = 0; index < n; index++)
-            System.out.println(index + "  |   }" + i[index] + "  |  " + f[index]);
+        for (int index = 0; index < n; index++) {
+            System.out.println(index + "  |  " + i[index] + "  |  " + f[index]);
+        }
+        System.out.println("suma: " + suma);
     }
 }
