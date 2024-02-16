1fe6007371478175f28de0c6a103767e2732c06e
Bruno <brunitosi123@gmail.com>
2024-02-16 13:38:33 -0600

    Clase serie1.1

--- src/ci/unidad1/aproximation/SerieTest.java
+++ src/ci/unidad1/aproximation/SerieTest.java
@@ -1,7 +1,21 @@
+/*
+* ITESS-TICS
+* Enero-Junio 2024
+* Cálculo Integral
+* TI202 BRUNO
+* 16 Febrero 2024
+* Cálculo de series Test
+* brunitosi123@gmail.com
+*/
 
 package ci.unidad1.aproximation;
 
 
 public class SerieTest {
-    
+    public static void main(String ... args){
+        Serie s = new Serie(1,25);
+        
+        s.compute();
+        s.print();
+    }    
 }
